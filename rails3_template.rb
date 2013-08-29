f016f4b7a556fc7465091e9419b37d36155a8f40
Masahiro Nishimi <nishimi@sonicgarden.jp>
2013-08-29 14:48:02 +0900

    fix filename

--- rails3_template.rb
+++ rails3_template.rb
@@ -75,7 +75,7 @@ get_and_store 'app/assets/stylesheets/colors/_colorlovers.css.sass'
 get_and_store 'app/assets/stylesheets/colors/_dom.css.sass'
 get_and_store 'app/assets/stylesheets/colors/_rambaral.css.sass'
 
-get_and_store 'app/assets/images/badge_grunge.png'
+get_and_store 'app/assets/images/bedge_grunge.png'
 empty_directory 'app/assets/images/ico'
 get_and_store 'app/assets/images/ico/favicon.ico'
 
